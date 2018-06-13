7042d3981f39a09c92ae93c6e233c66ea52b3d6e
wellyogui <welyogui@gmail.com>
2018-06-12 21:18:47 -0300

    Request created, showing list of cards

--- app/src/main/java/com/example/well/clashroyaleguide/view/fragments/CardFragment.kt
+++ app/src/main/java/com/example/well/clashroyaleguide/view/fragments/CardFragment.kt
@@ -3,11 +3,18 @@ package com.example.well.clashroyaleguide.view.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.well.clashroyaleguide.R
+import com.example.well.clashroyaleguide.model.Cards
+import com.example.well.clashroyaleguide.presenter.CardPresenter
+import com.example.well.clashroyaleguide.sync.contracts.CardListListener
+import com.example.well.clashroyaleguide.view.adapter.CardAdapter
+import kotlinx.android.synthetic.main.fragment_card.*
+import kotlinx.android.synthetic.main.fragment_card.view.*
 
 
 /**
@@ -15,10 +22,47 @@ import com.example.well.clashroyaleguide.R
  */
 class CardFragment: Fragment() {
 
+    lateinit var cardAdapter: CardAdapter
+    lateinit var cards: MutableList<Cards>
+    var cardPresenter = CardPresenter()
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card, container, false)
+        val view = inflater.inflate(R.layout.fragment_card, container, false)
+
+        val linearLayout = GridLayoutManager(context,4)
+        view.cardListView.layoutManager = linearLayout
+
+        getAllCards()
+
+        return view
     }
 
-}// Required empty public constructor
+    private fun getAllCards(){
+        cardPresenter.getAllCards(object : CardListListener {
+            override fun onCardListLoad(cardList: MutableList<Cards>) {
+                cards = cardList
+
+                cardAdapter = CardAdapter(cards)
+                cardAdapter.addAllCards(cards)
+                cardListView.adapter = cardAdapter
+            }
+
+            override fun onRequestStarted() {
+                Log.d("Start", "Start")
+            }
+
+            override fun onRequestFinished() {
+                Log.d("onRequestFinished", "finished")
+            }
+
+            override fun onError(error: Throwable) {
+                Log.d("onError", error.message)
+            }
+        }
+
+        )
+    }
+
+}
